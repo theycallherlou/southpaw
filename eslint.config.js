31b1df1907e63720a98218551536c72398108c04
Adam Robson <adamrayrobson@gmail.com>
2024-09-01 13:32:05 -0700

    add swa config
    Signed-off-by: Adam Robson <adamrayrobson@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,13 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import react from 'eslint-plugin-react'
 
 export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   {
-    settings: { react: { version: 'latest' } },
-    plugins: {
-      react
-    }
-  },
-  {
-    ignores: ['dist']
-  },
-  {
-    extends: [js.configs.recommended, 
-    ...tseslint.configs.recommendedTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked
-    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -28,19 +16,19 @@ export default tseslint.config(
         ecmaFeatures: { jsx: true },
         project: ['./tsconfig.app.json', './tsconfig.node.json'],
         sourceType: 'module',
-        tsconfigRootDir: import.meta.dirname
+        tsconfigRootfDir: '.'
       }
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      react
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ]
+    settings: { 
+      react: { 
+        version: 'latest' 
+      } 
     }
+  },
+  {
+    ignores: ['dist']
   }
 )
